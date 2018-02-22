99a6dc59c35cc0a44c04234a4b75b1e296ebc811
Jesper Kielsgaard <jkielsgaard@outlook.com>
2018-02-22 20:52:42 +0100

    Updating project with descriptions how to use this repo

--- CSharp_Console/Program.cs
+++ CSharp_Console/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CSharp_Console
 {
     class Program
     {
-        static void Main(string[] args)
+        /*
+         * Main will start run method so static is not needed when testing og debugging new codes
+         * Call the CodeSample file from the run method so this will be a clean code sample 
+         * 
+         * Remenber to document what the code due and make notes in codes
+         * 
+         * Make a new CS file in the "CodeSamples" folder and give it a descriptive name
+         * so it's easy to see what the purpose is with it.
+         * If there is a CS with that name, put the code inside that and make sure to make new
+         * method in it and make it easy to manageable
+         * 
+         * If the CodeSample file need a dll-file or others files as s CS-file from other 
+         * developer put them into the "Files" folder and call them from there
+         * 
+         * And remenber 
+         *   ___ _                              _      _          _  __ _        _    ___         _     
+         *  / __| |___ __ _ _ _    __ _ _ _  __| |  __| |__ _ _ _(_)/ _(_)___ __| |  / __|___  __| |___ 
+         * | (__| / -_) _` | ' \  / _` | ' \/ _` | / _| / _` | '_| |  _| / -_) _` | | (__/ _ \/ _` / -_)
+         *  \___|_\___\__,_|_||_| \__,_|_||_\__,_| \__|_\__,_|_| |_|_| |_\___\__,_|  \___\___/\__,_\___|                                                                                     
+         *  _      _  _                        ___         _             
+         * (_)___ | || |__ _ _ __ _ __ _  _   / __|___  __| |___        //  \\
+         * | (_-< | __ / _` | '_ \ '_ \ || | | (__/ _ \/ _` / -_)      _\\()//_  <-- beware of the bugs
+         * |_/__/ |_||_\__,_| .__/ .__/\_, |  \___\___/\__,_\___|     / //  \\ \
+         *                  |_|  |_|   |__/                            | \__/ |
+         *
+         */
+        static void Main(string[] args) { Program p = new Program(); p.run(); }
+
+        private void run()
         {
+
         }
     }
 }
