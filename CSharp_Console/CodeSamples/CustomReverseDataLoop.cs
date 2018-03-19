b1b515ad1b6a758d62dba71b9246504bfc057f18
Jesper Kielsgaard <jkielsgaard@outlook.com>
2018-03-19 21:07:56 +0100

    add descripsion to the code

--- CSharp_Console/CodeSamples/CustomReverseDataLoop.cs
+++ CSharp_Console/CodeSamples/CustomReverseDataLoop.cs
@@ -8,33 +8,42 @@ namespace CSharp_Console.CodeSamples
 {
     class CustomReverseDataLoop
     {
+        /* Made by Jesper Ahasverusen Kielsgaard
+         * Date: 02-03-2018
+         * 
+         * Reverse ForLoop that put data from position 2 to 3, 1 to 2 etc. beforce putting new data into position 0
+         */
+
         public void run()
         {
-            arrayint[] num = new arrayint[1444];
+            // I make 4 empty array CustomInt objects and call it num
+            CustomInt[] num = new CustomInt[4]; 
 
-            num[0] = new arrayint { x = 1, y = 1 };
-            num[1] = new arrayint { x = 2, y = 2 };
-            num[2] = new arrayint { x = 3, y = 3 };
-            num[3] = new arrayint { x = 4, y = 4 };
+            // Fill num with data
+            num[0] = new CustomInt { x = 1, y = 1 };
+            num[1] = new CustomInt { x = 2, y = 2 };
+            num[2] = new CustomInt { x = 3, y = 3 };
+            num[3] = new CustomInt { x = 4, y = 4 };
 
+            // This is only used to print out the data to see the sequences beforce the Forloop/Change
             int c = 0;
-            foreach (arrayint i in num) { if (i != null) { Console.WriteLine(c + "-" + i.x + "" + i.y); c++; } }
-
+            foreach (CustomInt i in num) { if (i != null) { Console.WriteLine(c + "-" + i.x + "" + i.y); c++; } }
             Console.WriteLine();
 
-            for (int i = 1443; i >= 0; i--)
+            // The forloop is a normal forloop i only reverse it and then it will take data from the i before the loop is on (i = i-1)
+            for (int i = 4; i >= 0; i--)
             {
-                if (i == 0) { num[i] = new arrayint { x = 0, y = 0 }; }
+                if (i == 0) { num[i] = new CustomInt { x = 0, y = 0 }; }
                 else { num[i] = num[i - 1]; }
             }
 
+            // This is only used to print out the data to see the sequences after the Forloop/Change
             c = 0;
-            foreach (arrayint i in num) { if (i != null) { Console.WriteLine(c + "-" + i.x + "" + i.y); c++; } }
-
+            foreach (CustomInt i in num) { if (i != null) { Console.WriteLine(c + "-" + i.x + "" + i.y); c++; } }
         }
     }
 
-    public class arrayint
+    public class CustomInt // Here a make a custom int that contian two int
     {
         public int x;
         public int y;
